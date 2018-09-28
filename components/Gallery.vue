9b3c7bc509f5748326ee8abf0cf7f2265efff150
Geidel Guerra <geidelguerra@gmail.com>
2018-09-28 17:20:18 -0400

    Remove unused classes

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -1,6 +1,6 @@
 <template lang="html">
   <div class="w-full h-full flex flex-col relative">
-    <div class="flex pin-t z-20 w-full p-2">
+    <div class="flex w-full p-2">
       <div class="text-white text-sm p-2">
         {{ value + 1 }} de {{ items.length }}
       </div>
@@ -19,7 +19,7 @@
       <button type="button" class="absolute pin-r pin-y w-1/3 bg-white opacity-0 focus:outline-none hover:opacity-10" @click="nextItem"></button>
     </div>
 
-    <div class="pl-4 pr-4 overflow-hidden z-10">
+    <div class="pl-4 pr-4 overflow-hidden">
       <transition name="slide-fade-top" mode="out-in">
         <h2 class="text-white text-center uppercase text-xl font-thin" :key="currentItem.image.url">{{ currentItem.title}}</h2>
       </transition>
